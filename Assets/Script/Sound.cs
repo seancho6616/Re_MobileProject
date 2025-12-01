c68ef560de8b464a7f343329b2972b94e3a54cc5
jinoodle8 <jh0308036@naver.com>
2025-12-02 02:41:21 +0900

    메인, 메뉴 사운드 추가

--- Assets/Script/Sound.cs
+++ Assets/Script/Sound.cs
@@ -1,14 +1,70 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Sound : MonoBehaviour
 {
-    AudioSource audioSource;
-    void Start()
+    private static Sound instance; // 싱글톤 인스턴스
+    private AudioSource audioSource;
+
+    [Header("BGM Clips")]
+    public AudioClip menuBGM; // 로그인 & 시작 화면용 음악
+    public AudioClip mainBGM; // 메인 게임용 음악
+
+    void Awake()
     {
-        audioSource = GetComponent<AudioSource>();
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            audioSource = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // 1. 재생해야 할 음악 결정
+        AudioClip targetClip = null;
+
+        if (scene.name == "LoginScene" || scene.name == "StartScene")
+        {
+            targetClip = menuBGM; // 메뉴 음악
+        }
+        else if (scene.name == "MainScene")
+        {
+            targetClip = mainBGM; // 게임 음악
+        }
+
+        // 2. 음악 교체 로직 
+        if (targetClip != null)
+        {
+            // Login -> Start
+            if (audioSource.clip != targetClip) 
+            {
+                audioSource.clip = targetClip;
+                audioSource.Play();
+            }
+        }
+    }
+
     public void PlaySound()
     {
-        audioSource.Play();
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
     }
 }
