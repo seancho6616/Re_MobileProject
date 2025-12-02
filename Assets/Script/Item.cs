fc8d2d9d63448137b88d90ae334f8c8a7f1580c1
seancho6616 <129930818+seancho6616@users.noreply.github.com>
2025-12-02 09:50:11 +0900

    코인 오디오추가

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -4,9 +4,17 @@ public class Item : MonoBehaviour
 {
     public ItemState itemState;
     [SerializeField] float rotateSpeed;
+    public AudioSource audioSource;
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);        
+    }
+    public void PlaySound()
+    {
+        audioSource.Play();
     }
 }
